cf53dffa542dec9f3ffec4d9b13922da09b8aa19
tvignesh-it <67905902+tvignesh-it@users.noreply.github.com>
2022-03-04 10:49:29 +0530

    Add files via upload
    Created the base project

---
+++ HHI Case Study/HHI Case Study/HHI_Case_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHI_Case_StudyApp.swift
+//  HHI Case Study
+//
+//  Created by Vignesh on 04/03/22.
+//
+
+import SwiftUI
+
+@main
+struct HHI_Case_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
